f9b57da3d1c00a9561a20b94841de7257d2f73b6
Arli Machado <arli.machado@gmail.com>
2023-08-21 19:24:02 -0300

    fix: corrigindo scripts de criacao das tabelas de pedido e item pedido

--- src/main/resources/db/migration/V006__cria-tabela-item-pedido.sql
+++ src/main/resources/db/migration/V006__cria-tabela-item-pedido.sql
@@ -2,11 +2,12 @@ create table item_pedido (
 	id bigint not null auto_increment,
 	produto_id bigint not null,
 	pedido_id bigint not null,
-	quantidade bigint not null,
+	quantidade smallint not null,
 	preco_unitario decimal(10,2) not null,
 	preco_total decimal(10,2) not null,
-	observacao varchar(20) not null,
+	observacao varchar(20),
   primary key (id),
+	unique key uk_item_pedido_produto (pedido_id, produto_id),
   constraint fk_item_pedido_produto foreign key (produto_id) references produto(id),
   constraint fk_item_pedido_pedido foreign key (pedido_id) references pedido(id)
 ) engine=InnoDB default charset=utf8;
