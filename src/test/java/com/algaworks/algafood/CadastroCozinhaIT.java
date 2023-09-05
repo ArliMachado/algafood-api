f093743328aa5b9b309db456fa17c0b7683f0a5d
Arli Machado <arli.machado@gmail.com>
2023-09-05 10:29:45 -0300

    Efetuando testes de integracao utilizando Rest assured

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,86 +1,72 @@
 package com.algaworks.algafood;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import java.math.BigDecimal;
-
-import javax.validation.ConstraintViolationException;
-
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.service.CadastroCozinhaService;
-import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-@SpringBootTest
+import org.flywaydb.core.Flyway;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 
-	@Autowired
-	private CadastroCozinhaService cadastroCozinha;
+	@LocalServerPort
+	private int port;
 
 	@Autowired
-	CadastroRestauranteService cadastroRestaurante;
+	private Flyway flyway;
 
-	@Test
-	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Chinesa");
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
 
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-
-		assertThat(novaCozinha).isNotNull();
-		assertThat(novaCozinha.getId()).isNotNull();
-	}
-
-	@Test
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome(null);
-
-		ConstraintViolationException erroEsperado = Assertions.assertThrows(ConstraintViolationException.class, () -> {
-			cadastroCozinha.salvar(novaCozinha);
-		});
-
-		assertThat(erroEsperado).isNotNull();
-	}
-
-	@Test
-	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Argentina");
-
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		Long cozinhaId = novaCozinha.getId();
-
-		Restaurante restaurante = new Restaurante();
-
-		restaurante.setNome("Cozinha Boa");
-		restaurante.setTaxaFrete(new BigDecimal(5));
-		restaurante.setCozinha(novaCozinha);
-
-		cadastroRestaurante.salvar(restaurante);
-
-		EntidadeEmUsoException erroEsperado = Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
-			cadastroCozinha.excluir(cozinhaId);
-		});
-
-		assertThat(erroEsperado).isNotNull();
+		flyway.migrate();
 
 	}
 
 	@Test
-	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-		CozinhaNaoEncontradaException erroEsperado = Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
-			cadastroCozinha.excluir(10000L);
-		});
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
 
-		assertThat(erroEsperado).isNotNull();
+		given()
+				.accept(ContentType.JSON)
+				.when()
+				.get()
+				.then()
+				.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+		given()
+				.accept(ContentType.JSON)
+				.when()
+				.get()
+				.then()
+				.body("", hasSize(4));
+	}
+
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+				.body("{ \"nome\": \"Chinesa\" }")
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.when()
+				.post()
+				.then()
+				.statusCode(HttpStatus.CREATED.value());
 	}
 
 }
